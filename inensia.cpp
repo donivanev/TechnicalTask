243dc8029916d0dc135ec6d2f158f2153284206a
Doni <dok246@gmail.com>
2022-07-20 16:13:03 +0300

    Update inensia.cpp

--- inensia.cpp
+++ inensia.cpp
@@ -6,27 +6,6 @@
 #include <sstream>
 using namespace std;
 
-/*
-We need to store anonymous data about programmers' salary and position.
-Create a binary file called salary.bin.
-Until there is input from the console write the data to the file.
-Then read the file and calculate the average salary of a position including a word read from the console. Round the result to 2 decimal places.
-
-Example:
-
-Input:
-
-Java Developer 4200
-C++ Developer 5000
-Javascript Developer 1000
-Junior C++ Developer 1500
-Software Engineer 3400
-Game Developer (C++) 4000
-C++
-
-Output: Average C++ salary is: 7833,33
-*/
-
 int main()
 {
     fstream file;
